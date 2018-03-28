07552891b706b1a77a301bd6df8a10c480cd00bc
zzp <2269225970@qq.com>
2018-03-28 21:08:51 +0800

    Change the if...else... structure

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -7,20 +7,19 @@ import java.util.Map;
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-        Map<String,Integer> result = new HashMap<String, Integer>();
+        Map<String, Integer> result = new HashMap<String, Integer>();
         for (String obj : collection1) {
-            if (result.containsKey(obj)) {
-                result.put(obj, result.get(obj).intValue() + 1);
-            } else {
-                if(obj.length()>1){
-                    String[] strArr = obj.split("[-:\\[\\]]");
-                    if(result.containsKey(strArr[0])){
-                        result.put(strArr[0],  Integer.valueOf(strArr[1]) + result.get(strArr[0]).intValue());
-                    }else{
-                        result.put(strArr[0], Integer.valueOf(strArr[1]));
-                    }
+            if (obj.length() > 1) {
+                String[] strArr = obj.split("[-:\\[\\]]");
+                if (result.containsKey(strArr[0])) {
+                    result.put(strArr[0], Integer.valueOf(strArr[1]) + result.get(strArr[0]).intValue());
+                } else {
+                    result.put(strArr[0], Integer.valueOf(strArr[1]));
                 }
-                else {
+            } else {
+                if (result.containsKey(obj)) {
+                    result.put(obj, result.get(obj).intValue() + 1);
+                } else {
                     result.put(obj, 1);
                 }
             }
